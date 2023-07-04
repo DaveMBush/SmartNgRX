c6b173eaaa8dc819ffdb42c50fe9be7836619aa6
Dave Bush <dave@davembush.com>
2023-07-04 17:35:28 -0400

    docs: add contributor guidlines (#32)
    This adds the start of contributor guidelines.

--- apps/dynamic-ngrx-test/src/app/app.routes.ts
+++ apps/dynamic-ngrx-test/src/app/app.routes.ts
@@ -7,15 +7,19 @@ import { TodoService } from './state/todo.service';
 
 const todoService = new InjectionToken<TodoService>('todoService');
 
-export const appRoutes: Route[] = [{
-  path: '',
-  pathMatch: 'full',
-  redirectTo: 'list',
-},{
-  path: 'list',
-  loadComponent: async () => (await import('./list/list.component')).ListComponent,
-  providers: [
-    { provide: todoService, useClass: TodoService},
-    provideSlice('list','todo', todoService ),
-  ]
-}];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list',
+  },
+  {
+    path: 'list',
+    loadComponent: async () =>
+      (await import('./list/list.component')).ListComponent,
+    providers: [
+      { provide: todoService, useClass: TodoService },
+      provideSlice('list', 'todo', todoService),
+    ],
+  },
+];
