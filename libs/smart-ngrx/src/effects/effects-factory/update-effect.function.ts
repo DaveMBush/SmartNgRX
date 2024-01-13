8c6f898b6237a51e5cf46775f62dd60e64855161
Dave Bush <dave@davembush.com>
2024-01-13 17:08:31 -0500

    embellish optimistic update

--- libs/smart-ngrx/src/effects/effects-factory/update-effect.function.ts
+++ libs/smart-ngrx/src/effects/effects-factory/update-effect.function.ts
@@ -1,10 +1,12 @@
 import { inject, InjectionToken } from '@angular/core';
 import { Actions, ofType } from '@ngrx/effects';
-import { concatMap, map } from 'rxjs';
+import { concatMap, debounceTime, map, tap } from 'rxjs';
 
+import { castTo } from '../../common/cast-to.function';
 import { ActionGroup } from '../../functions/action-group.interface';
 import { MarkAndDelete } from '../../types/mark-and-delete.interface';
 import { EffectService } from '../effect-service';
+import { manageMaps } from './update-effect/manage-maps.function';
 
 /**
  * this handles the update by calling the effectService.update()
@@ -25,6 +27,8 @@ export function updateEffect<T extends MarkAndDelete>(
   effectServiceToken: InjectionToken<EffectService<T>>,
   actions: ActionGroup<T>,
 ) {
+  const lastRow: Map<string, T> = new Map();
+  const lastRowTimeout: Map<string, number> = new Map();
   return (
     /* istanbul ignore next -- default value, not really a condition */
     actions$ = inject(Actions),
@@ -33,10 +37,26 @@ export function updateEffect<T extends MarkAndDelete>(
   ) => {
     return actions$.pipe(
       ofType(actions.update),
+      tap((action) => {
+        manageMaps<T>(lastRow, lastRowTimeout, action);
+      }),
+      // debounceTime(1) lets us set multiple fields in a row but only
+      // call the server once
+      debounceTime(1),
       concatMap((action) => {
-        return effectService.update(action.old.row, action.new.row);
+        const id = castTo<{ id: string }>(action.old.row).id;
+        return effectService.update(lastRow.get(id)!, action.new.row);
       }),
       map((rows) => {
+        // set the last row to the row we got back here.
+        // rows only has one row it it we just return an array
+        // so we can reuse code.
+        const now = Date.now();
+        const id = castTo<{ id: string }>(rows[0]).id;
+        // delete the timeout to keep things in order.
+        lastRowTimeout.delete(id);
+        lastRowTimeout.set(id, now);
+        lastRow.set(id, rows[0]);
         return actions.loadByIdsSuccess({ rows });
       }),
     );
---
+++ libs/smart-ngrx/src/effects/effects-factory/update-effect/manage-maps.function.ts
@@ -0,0 +1,42 @@
+import { castTo } from '../../../common/cast-to.function';
+import { RowProp } from '../../../types/row-prop.interface';
+
+/**
+ * Helper function that updates the maps used in update-effect.function.ts
+ * so we can implement optimistic updates and can rollback on error when
+ * needed.
+ *
+ * @param lastRow the map of the last row sent to the server
+ * @param lastRowTimeout the map of the last time a row was sent to the server
+ * @param action the action that was dispatched
+ * @param action.old the row the new row is replacing
+ * @param action.new the new row we are sending to the server
+ */
+export function manageMaps<T>(
+  lastRow: Map<string, T>,
+  lastRowTimeout: Map<string, number>,
+  action: {
+    old: RowProp<T>;
+    new: RowProp<T>;
+  },
+): void {
+  const now = Date.now();
+  const keys = Array.from(lastRowTimeout.keys());
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const timeStamp = lastRowTimeout.get(key)!;
+    // break out when there are no longer items to process
+    // because we use a map, items are in time order.
+    if (now - timeStamp < 60000) {
+      break;
+    }
+    lastRowTimeout.delete(key);
+    lastRow.delete(key);
+  }
+  // for now we assume everything has a primary key of id.
+  const id = castTo<{ id: string }>(action.old.row).id;
+  if (!lastRowTimeout.has(id)) {
+    lastRowTimeout.set(id, now);
+    lastRow.set(id, action.old.row);
+  }
+}
