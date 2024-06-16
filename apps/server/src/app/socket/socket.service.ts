5855fc7ea13900b333137e82c41ac255e296efd5
Dave Bush <dave@davembush.com>
2024-06-16 09:01:51 -0400

    strongly type socket.on handler

--- apps/server/src/app/socket/socket.service.ts
+++ apps/server/src/app/socket/socket.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 
+import { CUDMessage } from './cud-message.interface';
+
 @Injectable()
 export class SocketService {
   handleConnection(socket: Socket): void {
-    socket.on('emit', (data) => {
+    socket.on('emit', (data: CUDMessage) => {
       socket.emit('message', data);
     });
   }
