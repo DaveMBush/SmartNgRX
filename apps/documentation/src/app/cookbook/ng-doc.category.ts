24b28054bb221844a43abdd27557da355da64341
Dave Bush <dave@davembush.com>
2024-06-18 10:20:42 -0400

    websocket documentation

--- apps/documentation/src/app/cookbook/ng-doc.category.ts
+++
@@ -1,9 +0,0 @@
-import { NgDocCategory } from '@ng-doc/core';
-
-const CookbookNgRXCategory: NgDocCategory = {
-  title: 'Cookbook',
-  expandable: true,
-  order: 3,
-};
-
-export default CookbookNgRXCategory;
