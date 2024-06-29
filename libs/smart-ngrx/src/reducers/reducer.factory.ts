7ec0abd0158ea8e97592f92599824f56117c5380
Dave Bush <dave@davembush.com>
2024-06-29 18:48:48 -0400

    remove castTo from reducerFactory

--- libs/smart-ngrx/src/reducers/reducer.factory.ts
+++ libs/smart-ngrx/src/reducers/reducer.factory.ts
@@ -2,7 +2,6 @@ import { EntityState } from '@ngrx/entity';
 import { ActionReducer, createReducer, on } from '@ngrx/store';
 
 import { actionFactory } from '../actions/action.factory';
-import { castTo } from '../common/cast-to.function';
 import { StringLiteralSource } from '../ngrx-internals/string-literal-source.type';
 import { entityDefinitionCache } from '../registrations/entity-definition-cache.function';
 import { SmartNgRXRowBase } from '../types/smart-ngrx-row-base.interface';
@@ -24,28 +23,24 @@ export function reducerFactory<
   feature: StringLiteralSource<F>,
   entity: StringLiteralSource<E>,
 ): ActionReducer<EntityState<T>> {
-  const adapter = entityDefinitionCache(feature, entity).entityAdapter;
+  const adapter = entityDefinitionCache<T>(feature, entity).entityAdapter;
   const initialState = adapter.getInitialState();
   const actions = actionFactory<T, F, E>(feature, entity);
 
-  return castTo<ActionReducer<EntityState<T>>>(
-    createReducer(
-      initialState,
-      on(actions.add, (state, { row }) => adapter.upsertOne(row, state)),
-      on(actions.addSuccess, (state, { newRow }) =>
-        adapter.upsertOne(newRow, state),
-      ),
-      on(actions.updateMany, (state, { changes }) => {
-        return adapter.updateMany(changes, state);
-      }),
-      on(actions.remove, (state, { ids }) => adapter.removeMany(ids, state)),
-      on(actions.update, (state, { new: { row } }) =>
-        adapter.upsertOne(row, state),
-      ),
-
-      on(actions.storeRows, (state, { rows }) =>
-        adapter.upsertMany(rows, state),
-      ),
+  return createReducer(
+    initialState,
+    on(actions.add, (state, { row }) => adapter.upsertOne(row, state)),
+    on(actions.addSuccess, (state, { newRow }) =>
+      adapter.upsertOne(newRow, state),
     ),
+    on(actions.updateMany, (state, { changes }) => {
+      return adapter.updateMany(changes, state);
+    }),
+    on(actions.remove, (state, { ids }) => adapter.removeMany(ids, state)),
+    on(actions.update, (state, { new: { row } }) =>
+      adapter.upsertOne(row, state),
+    ),
+
+    on(actions.storeRows, (state, { rows }) => adapter.upsertMany(rows, state)),
   );
 }
