ccfc79559425220a794606ea1c8cf234507af374
Dave Bush <dave@davembush.com>
2024-08-12 10:12:21 -0400

    fix unit tests

--- libs/smart-ngrx/src/selector/convert-children-to-virtual-array.function.spec.ts
+++ libs/smart-ngrx/src/selector/convert-children-to-virtual-array.function.spec.ts
@@ -1,7 +1,6 @@
 import { EntityState } from '@ngrx/entity';
 
 import { ActionGroup } from '../actions/action-group.interface';
-import { ChildType } from '../types/child-type.type';
 import { SmartNgRXRowBase } from '../types/smart-ngrx-row-base.interface';
 import { VirtualArrayContents } from '../types/virtual-array-contents.interface';
 import { convertChildrenToVirtualArray } from './convert-children-to-virtual-array.function';
@@ -12,13 +11,11 @@ describe('convertChildrenToVirtualArray', () => {
     field: VirtualArray<Parent> | VirtualArrayContents;
   }
 
-  let children: Partial<Record<keyof Parent, ChildType>>;
   let parentFieldName: keyof Parent;
   let parentEntity: EntityState<Parent>;
   let parentAction: ActionGroup;
 
   beforeEach(() => {
-    children = { field: 'virtual' };
     parentFieldName = 'field';
     parentEntity = {
       ids: ['1'],
@@ -30,26 +27,18 @@ describe('convertChildrenToVirtualArray', () => {
   });
 
   it('should convert field to VirtualArray when children[parentFieldName] is "virtual"', () => {
-    convertChildrenToVirtualArray(
-      children,
-      parentFieldName,
-      parentEntity,
-      parentAction,
-    );
+    convertChildrenToVirtualArray(parentFieldName, parentEntity, parentAction);
 
     const row = parentEntity.entities['1'];
     expect(row?.field).toBeInstanceOf(VirtualArray);
   });
 
-  it('should not convert field to VirtualArray when children[parentFieldName] is not "virtual"', () => {
-    children = { field: 'fixed' };
-
-    convertChildrenToVirtualArray(
-      children,
-      parentFieldName,
-      parentEntity,
-      parentAction,
-    );
+  it('should not convert field to VirtualArray when row[childField] is an Array', () => {
+    const field = parentEntity.entities['1'];
+    if (field) {
+      field.field = [] as unknown as VirtualArrayContents;
+    }
+    convertChildrenToVirtualArray(parentFieldName, parentEntity, parentAction);
 
     const row = parentEntity.entities['1'];
     expect(row?.field).not.toBeInstanceOf(VirtualArray);
