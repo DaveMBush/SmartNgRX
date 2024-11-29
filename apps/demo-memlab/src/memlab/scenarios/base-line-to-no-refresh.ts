6fcdc86c8da738d38b8259419dd6cc19b7a78afd
Dave Bush <dave@davembush.com>
2024-11-29 12:10:40 -0500

    remove memlab

--- apps/demo-memlab/src/memlab/scenarios/base-line-to-no-refresh.ts
+++
@@ -1,6 +0,0 @@
-import { baseLineToTab } from './base-line-to-tab.factory';
-
-export const scenario = baseLineToTab(
-  'treeNoRefresh',
-  'base-line-to-no-refresh',
-);
--- apps/demo-memlab/src/memlab/src/work-directory.function.ts
+++
@@ -1,9 +0,0 @@
-let workDir = '';
-
-export function workDirectory(dir?: string): string {
-  /* istanbul ignore next -- trivial*/
-  if (dir !== undefined) {
-    workDir = dir;
-  }
-  return workDir;
-}
