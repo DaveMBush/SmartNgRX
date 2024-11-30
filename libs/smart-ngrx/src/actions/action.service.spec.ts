514316dfa9cc441eecada788a3d15f217d7acc42
Dave Bush <dave@davembush.com>
2024-11-30 16:35:05 -0500

    fixed spec file for lint

--- libs/smart-ngrx/src/actions/action.service.spec.ts
+++ libs/smart-ngrx/src/actions/action.service.spec.ts
@@ -124,7 +124,6 @@ describe('ActionService', () => {
         };
 
         // Mock the store's select method to return our mock entities
-        // eslint-disable-next-line deprecation/deprecation -- needed for testing
         mockStore.select.mockReturnValue(of(mockEntities));
 
         // Mock the entityAdapter's selectEntities method
@@ -155,7 +154,6 @@ describe('ActionService', () => {
         const mockEntities: Record<string, MockEntity> = {
           '1': { id: '1', isEditing: true },
         };
-        // eslint-disable-next-line deprecation/deprecation -- needed for testing
         mockStore.select.mockReturnValue(of(mockEntities));
 
         service.garbageCollect(['1']);
