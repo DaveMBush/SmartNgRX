f0aae1752a87b55efbb8ce2526b7341f47228c5d
Dave Bush <dave@davembush.com>
2025-03-11 10:29:33 -0400

    updated typescript-eslint and eslint-plugin-sonarjs and fixed code for new rules

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line sonarjs/no-commented-code -- see below
 //import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
@@ -5,6 +6,7 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 // we only need this for localhost dev tools support
+// eslint-disable-next-line sonarjs/no-commented-code -- see above
 //enableProdMode()
 
 function catchError(err: unknown): void {
