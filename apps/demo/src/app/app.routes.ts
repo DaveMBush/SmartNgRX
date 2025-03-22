25da30275459298560e0a0780a6f64dbf3dd9ea7
Dave Bush <dave@davembush.com>
2025-03-22 14:56:48 -0400

    cleanup

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -34,7 +34,6 @@ import { standardSignalsDepartmentsDefinition } from './routes/tree-standard-sig
 import { standardSignalsDepartmentChildrenDefinition } from './routes/tree-standard-signals/store/department-children/standard-signals-department-children-definition';
 import { standardSignalsLocationsDefinition } from './routes/tree-standard-signals/store/locations/standard-signals-locations-definition';
 import { standardSignalsTopDefinition } from './routes/tree-standard-signals/store/top/standard-signals-top-definition.const';
-//import { currentLocationSignalStore } from './routes/tree-standard-signals/store/current-location/current-location.signal-store';
 
 // This ensure we have one key per SharedState property
 const sharedReducersStandard: ActionReducerMap<TreeStandardState2> = {
