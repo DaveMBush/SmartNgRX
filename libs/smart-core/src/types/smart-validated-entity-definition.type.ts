470def43354d06dd18e64863963d693a5319235f
Dave Bush <dave@davembush.com>
2025-04-17 14:02:13 -0400

    more tests

--- libs/smart-core/src/types/smart-validated-entity-definition.type.ts
+++ libs/smart-core/src/types/smart-validated-entity-definition.type.ts
@@ -7,5 +7,9 @@ import { SmartNgRXRowBase } from './smart-ngrx-row-base.interface';
  * We need a type that makes the entityAdapter field in SmartEntityDefinition
  * not optional once it has been provided by the code. This is that type.
  */
+interface PublicEntityAdapter<Row extends SmartNgRXRowBase> {
+  entityAdapter: EntityAdapter<Row>;
+}
+
 export type SmartValidatedEntityDefinition<Row extends SmartNgRXRowBase> =
-  SmartEntityDefinition<Row> & { entityAdapter: EntityAdapter<Row> });
+  PublicEntityAdapter<Row> & SmartEntityDefinition<Row>;
--- libs/smart-ngrx/src/test-setup.ts
+++ libs/smart-ngrx/src/test-setup.ts
@@ -1,7 +1,4 @@
-import 'jest-preset-angular/setup-jest';
-
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
-
 setupZoneTestEnv({
   errorOnUnknownElements: true,
   errorOnUnknownProperties: true,
