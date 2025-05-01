5482ba21d9ab25ffbd8d178ad0ef18bbe9830c2b
Dave Bush <dave@davembush.com>
2025-05-01 14:59:11 -0400

    fixed NgDocPage name

--- apps/documentation/src/app/using-smart-signals/crud-support/retrieving/ng-doc.page.ts
+++ apps/documentation/src/app/using-smart-signals/crud-support/retrieving/ng-doc.page.ts
@@ -1,11 +1,11 @@
 import { NgDocPage } from '@ng-doc/core';
 import CrudSupportCategory from '../ng-doc.category';
 
-const UpdatesPage: NgDocPage = {
+const RetrievingPage: NgDocPage = {
   title: `Retrieving Rows`,
   mdFile: './index.md',
   order: 1,
   category: CrudSupportCategory,
 };
 
-export default UpdatesPage;
+export default RetrievingPage;
