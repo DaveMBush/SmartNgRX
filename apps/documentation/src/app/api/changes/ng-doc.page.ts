892255211c0c518930ea7924ccdcad3fc793ba39
Dave Bush <dave@davembush.com>
2025-05-05 20:56:34 -0400

    API Changes documentation

---
+++ apps/documentation/src/app/api/changes/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import APICategory from '../ng-doc.category';
+
+const ApiChangesPage: NgDocPage = {
+  title: `API Changes`,
+  mdFile: './index.md',
+  order: 4,
+  category: APICategory,
+};
+
+export default ApiChangesPage;
