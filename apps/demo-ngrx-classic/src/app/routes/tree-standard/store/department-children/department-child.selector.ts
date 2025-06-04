e8dff1e5b65f591bca9fdfe48868fe4d1631ad8c
Dave Bush <dave@davembush.com>
2025-06-04 19:11:05 -0400

    PR comment

--- apps/demo-ngrx-classic/src/app/routes/tree-standard/store/department-children/department-child.selector.ts
+++ apps/demo-ngrx-classic/src/app/routes/tree-standard/store/department-children/department-child.selector.ts
@@ -1,6 +1,8 @@
 import { createSmartSelector } from '@smarttools/smart-ngrx';
 
-export const selectDepartmentChildren = createSmartSelector(
+import { DepartmentChild } from '../../../../shared/department-children/department-child.interface';
+
+export const selectDepartmentChildren = createSmartSelector<DepartmentChild>(
   'tree-standard',
   'departmentChildren',
 );
