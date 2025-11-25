1844ad15c677857dc7b3d3763c53c2937487e812
Dave Bush <dave@davembush.com>
2025-11-25 18:31:09 -0500

    fix postinstall

--- apps/server/src/app/orm/prisma.const.ts
+++ apps/server/src/app/orm/prisma.const.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+import { adapter } from './prisma-adapter';
+
+export const prisma = new PrismaClient({ adapter });
